385b669976c90aae4f337ca65e7c67a8ac2f6735
Juraci Paixão Kröhling <juraci@kroehling.de>
2024-06-04 03:10:16 +1200

    [confighttp] Apply MaxRequestBodySize to the result of a decompressed body (#10289)
    This change applies a restriction on the size of the decompressed
    payload. Before this change, this is the result of the test added in
    this PR:
    
    ```
    --- FAIL: TestServerWithDecompression (0.03s)
        /home/jpkroehling/Projects/src/github.com/open-telemetry/opentelemetry-collector/config/confighttp/confighttp_test.go:1327:
            	Error Trace:	/home/jpkroehling/Projects/src/github.com/open-telemetry/opentelemetry-collector/config/confighttp/confighttp_test.go:1327
            	            				/usr/lib/golang/src/net/http/server.go:2166
            	            				/home/jpkroehling/Projects/src/github.com/open-telemetry/opentelemetry-collector/config/confighttp/compression.go:163
            	            				/home/jpkroehling/Projects/src/github.com/open-telemetry/opentelemetry-collector/config/confighttp/confighttp.go:455
            	            				/usr/lib/golang/src/net/http/server.go:2166
            	            				/home/jpkroehling/go/pkg/mod/go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp@v0.52.0/handler.go:212
            	            				/home/jpkroehling/go/pkg/mod/go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp@v0.52.0/handler.go:73
            	            				/usr/lib/golang/src/net/http/server.go:2166
            	            				/home/jpkroehling/Projects/src/github.com/open-telemetry/opentelemetry-collector/config/confighttp/clientinfohandler.go:26
            	            				/usr/lib/golang/src/net/http/server.go:3137
            	            				/usr/lib/golang/src/net/http/server.go:2039
            	            				/usr/lib/golang/src/runtime/asm_amd64.s:1695
            	Error:      	An error is expected but got nil.
            	Test:       	TestServerWithDecompression
        /home/jpkroehling/Projects/src/github.com/open-telemetry/opentelemetry-collector/config/confighttp/confighttp_test.go:1328:
            	Error Trace:	/home/jpkroehling/Projects/src/github.com/open-telemetry/opentelemetry-collector/config/confighttp/confighttp_test.go:1328
            	            				/usr/lib/golang/src/net/http/server.go:2166
            	            				/home/jpkroehling/Projects/src/github.com/open-telemetry/opentelemetry-collector/config/confighttp/compression.go:163
            	            				/home/jpkroehling/Projects/src/github.com/open-telemetry/opentelemetry-collector/config/confighttp/confighttp.go:455
            	            				/usr/lib/golang/src/net/http/server.go:2166
            	            				/home/jpkroehling/go/pkg/mod/go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp@v0.52.0/handler.go:212
            	            				/home/jpkroehling/go/pkg/mod/go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp@v0.52.0/handler.go:73
            	            				/usr/lib/golang/src/net/http/server.go:2166
            	            				/home/jpkroehling/Projects/src/github.com/open-telemetry/opentelemetry-collector/config/confighttp/clientinfohandler.go:26
            	            				/usr/lib/golang/src/net/http/server.go:3137
            	            				/usr/lib/golang/src/net/http/server.go:2039
            	            				/usr/lib/golang/src/runtime/asm_amd64.s:1695
            	Error:
            	Test:       	TestServerWithDecompression
        /home/jpkroehling/Projects/src/github.com/open-telemetry/opentelemetry-collector/config/confighttp/confighttp_test.go:1357:
            	Error Trace:	/home/jpkroehling/Projects/src/github.com/open-telemetry/opentelemetry-collector/config/confighttp/confighttp_test.go:1357
            	Error:      	Not equal:
            	            	expected: 200
            	            	actual  : 400
            	Test:       	TestServerWithDecompression
    FAIL
    FAIL	go.opentelemetry.io/collector/config/confighttp	0.036s
    FAIL
    ```
    
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>
    
    ---------
    
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- config/confighttp/confighttp.go
+++ config/confighttp/confighttp.go
@@ -30,6 +30,7 @@ import (
 )
 
 const headerContentEncoding = "Content-Encoding"
+const defaultMaxRequestBodySize = 20 * 1024 * 1024 // 20MiB
 
 // ClientConfig defines settings for creating an HTTP client.
 type ClientConfig struct {
@@ -272,7 +273,7 @@ type ServerConfig struct {
 	// RateLimit for this receiver
 	RateLimit *RateLimit `mapstructure:"rate_limit"`
 
-	// MaxRequestBodySize sets the maximum request body size in bytes
+	// MaxRequestBodySize sets the maximum request body size in bytes. Default: 20MiB.
 	MaxRequestBodySize int64 `mapstructure:"max_request_body_size"`
 
 	// IncludeMetadata propagates the client metadata from the incoming requests to the downstream consumers
@@ -343,7 +344,11 @@ func (hss *ServerConfig) ToServer(_ context.Context, host component.Host, settin
 		o(serverOpts)
 	}
 
-	handler = httpContentDecompressor(handler, serverOpts.errHandler, serverOpts.decoders)
+	if hss.MaxRequestBodySize <= 0 {
+		hss.MaxRequestBodySize = defaultMaxRequestBodySize
+	}
+
+	handler = httpContentDecompressor(handler, hss.MaxRequestBodySize, serverOpts.errHandler, serverOpts.decoders)
 
 	if hss.MaxRequestBodySize > 0 {
 		handler = maxRequestBodySizeInterceptor(handler, hss.MaxRequestBodySize)
